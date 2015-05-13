14180311ea0742948ea50e1dbc2bc96919d102b7
Clairebi <bixia.claire.si@gmail.com>
2015-05-12 22:48:34 -0700

    update db_con.php : connection info

--- php/db_con.php
+++ php/db_con.php
@@ -1,5 +1,5 @@
 <?php
-$db_conx = mysqli_connect("localhost:8888", "root", "root", "change36_wop");
+$db_conx = mysqli_connect("localhost", "root", "root", "smart_meter_reading","8888");
 // Evaluate the connection
 if (mysqli_connect_errno()) {
     echo mysqli_connect_error();
