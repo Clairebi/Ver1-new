1eaa700d6a76666aa52d52fd8136df200edf5de4
Clairebi <bixia.claire.si@gmail.com>
2015-05-19 14:03:28 -0700

    update php/init.php : change pg to mysql

--- php/init.php
+++ php/init.php
@@ -1,10 +1,10 @@
 <?php
 include_once("db_con_pg.php");
 $query = "SELECT username, init_conspt AS conspt, cycle_endpoint AS ce FROM users WHERE username = '$u'";
-$result = pg_query($query);
-$user_row = pg_fetch_array($result, 0, PGSQL_NUM);
+$result = mysqli_query($query);
+$user_row = mysqli_fetch_array($result, MYSQLI_NUM);
 //echo $user_row[0]; echo " "; echo $user_row[1]; echo " "; echo $user_row[2];
 
 $init = "SELECT init('$user_row[0]', $user_row[1], date '$user_row[2]')";
-$result11 = pg_query($init);
+$result11 = mysqli_query($init);
 ?>
